83d1a065e8332d729ef41aad95e2778cd6682d6f
= <=>
2023-02-21 10:11:42 +0100

    Ajout du genre a la page utilisateur

--- resources/views/pages/user.blade.php
+++ resources/views/pages/user.blade.php
@@ -24,11 +24,27 @@ $user = $userController->GetUserByName($name);
     <div class="d-flex">
         @include('components/header')
         <div class="w-75 mx-auto d-flex flex-column align-content-center" style="margin-left: 300px !important;">
-            <div class="d-flex align-items-end m-3">
-                <div class="rounded-circle ratio ratio-1x1 overflow-hidden" style="width: 3em;">
-                    <img src="{{ $user['picture'] }}" alt="">
+            <div class="d-flex justify-content-between align-items-end m-3">
+                <div class="d-flex">
+                    <div class="rounded-circle ratio ratio-1x1 overflow-hidden" style="width: 3em;">
+                        <img src="{{ $user['picture'] }}" alt="">
+                    </div>
+                    <h2>{{ $user['name'] }}</h2>
                 </div>
-                <h2>{{ $user['name'] }}</h2>
+                <?php
+                switch ($user['gender']) {
+                    case 'male':
+                        $gender = 'male';
+                        break;
+                    case 'female':
+                        $gender = 'female';
+                        break;
+                    default:
+                        $gender = 'help';
+                        break;
+                }
+                ?>
+                <img src="https://img.icons8.com/office/32/null/{{ $gender }}.png"/>
             </div>
             @if (Auth::check())
             <hr>
