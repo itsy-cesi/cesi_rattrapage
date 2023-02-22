dec9d542ebd80dbbec15d5e8e71ad93d370778b0
itsy <itsy@pandacrp.com>
2023-02-22 22:11:29 +0100

    Modification des cdn

--- resources/views/imports/cdns.blade.php
+++ resources/views/imports/cdns.blade.php
@@ -1,21 +1,38 @@
-<script src='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/js/bootstrap.bundle.min.js'
-    integrity='sha512-i9cEfJwUwViEPFKdC1enz4ZRGBj8YQo6QByFTF92YXHi7waCqyexvRD75S5NVTsSiTv7rKWqG9Y5eFxmRsOn0A=='
-    crossorigin='anonymous'></script>
-<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.3/jquery.js'
-    integrity='sha512-nO7wgHUoWPYGCNriyGzcFwPSF+bPDOR+NvtOYy2wMcWkrnCNPKBcFEkU80XIN14UVja0Gdnff9EmydyLlOL7mQ=='
-    crossorigin='anonymous'></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.min.js"></script>
-<script src='https://cdnjs.cloudflare.com/ajax/libs/noty/3.1.4/noty.js'
-    integrity='sha512-mgZL3SZ/vIooDg2mU2amX6NysMlthFl/jDbscSRgF/k3zmICLe6muAs7YbITZ+61FeUoo1plofYAocoR5Sa1rQ=='
-    crossorigin='anonymous'></script>
+@if (in_array('bootstrap', $required))
+    <script src='{{ URL::asset('/js/bootstrap.bundle.min.js') }}'></script>
+    <link rel='stylesheet' href='{{ URL::asset('/css/bootstrap.css') }}' />
+@endif
+
+@if (in_array('noty', $required))
+    <script src='{{ URL::asset('/js/noty.js') }}'></script>
+    <link rel='stylesheet' href='{{ URL::asset('/css/noty.css') }}' />
+@endif
 
 
-<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/noty/3.1.4/noty.css'
-    integrity='sha512-NXUhxhkDgZYOMjaIgd89zF2w51Mub53Ru3zCNp5LTlEzMbNNAjTjDbpURYGS5Mop2cU4b7re1nOIucsVlrx9fA=='
-    crossorigin='anonymous' />
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.css"
-    integrity="sha512-bR79Bg78Wmn33N5nvkEyg66hNg+xF/Q8NA8YABbj+4sBngYhv9P8eum19hdjYcY7vXk/vRkhM3v/ZndtgEXRWw=="
-    crossorigin="anonymous" />
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.css"
-    integrity="sha512-+ouAqATs1y4kpPMCHfKHVJwf308zo+tC9dlEYK9rKe7kiP35NiP+Oi35rCFnc16zdvk9aBkDUtEO3tIPl0xN5w=="
-    crossorigin="anonymous" />
+@if (in_array('font-awesome', $required))
+    <link rel='stylesheet' href='{{ URL::asset('/css/font-awesome.css') }}' />
+@endif
+
+
+@if (in_array('jquery', $required))
+    <script src='{{ URL::asset('/js/jquery.js') }}'></script>
+    <script src='{{ URL::asset('/js/jquery.cookie.js') }}'></script>
+@endif
+
+
+@if (in_array('textarea', $required))
+    <script src='{{ URL::asset('js/textarea.js') }}' defer></script>
+@endif
+
+@if (in_array('make_post', $required))
+    <script src='{{ URL::asset('js/make_post.js') }}' defer></script>
+@endif
+
+@if (in_array('post_action', $required))
+    <script src='{{ URL::asset('js/post_action.js') }}' defer></script>
+@endif
+
+
+@if (in_array('textarea', $required))
+    <link rel='stylesheet' href='{{ URL::asset('css/textarea.css') }}'>
+@endif
--- routes/web.php
+++ routes/web.php
@@ -5,25 +5,30 @@ use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\AuthentificatorController;
 use App\Http\Controllers\UserController;
 
-Route::get('/', function () {
-    return view('pages/home');
+Route::get('/', function ()
+{
+    return view('pages/home', ['required' => ['bootstrap', 'make_post', 'jquery', 'post_action', 'noty', 'font-awesome']]);
 })->name('home');
 
 
-Route::get('/login', function () {
-    return Auth::check() ? redirect('/') : view('pages/login');
+Route::get('/login', function ()
+{
+    return Auth::check() ? redirect('/') : view('pages/login', ['required' => ['bootstrap', 'register_form', 'jquery', 'noty', 'font-awesome']]);
 })->name('login');
 
-Route::get('/post/{id}', function ($id) {
-    return view('pages/post', ['id' => $id]);
+Route::get('/post/{id}', function ($id)
+{
+    return view('pages/post', ['id' => $id, 'required' => ['bootstrap', 'make_post', 'jquery', 'post_action', 'noty', 'font-awesome']]);
 })->name('post');
 
-Route::get('/user/{name}', function ($name) {
-    return view('pages/user', ['name' => $name]);
+Route::get('/user/{name}', function ($name)
+{
+    return view('pages/user', ['name' => $name, 'required' => ['bootstrap', 'make_post', 'jquery', 'post_action', 'noty', 'font-awesome']]);
 })->name('user');
 
-Route::get('/settings', function () {
-    return view('pages/settings');
+Route::get('/settings', function ()
+{
+    return view('pages/settings', ['required' => ['bootstrap', 'jquery', 'font-awesome']]);
 })->name('setting');
 
 Route::any(
